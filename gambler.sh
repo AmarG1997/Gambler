e904f36ec81c5ed5d822637dc6e8ad867913336b
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-11 14:43:31 +0530

    Added : Calculated 20 Days Total Wins Or Loss

--- gambler.sh
+++ gambler.sh
@@ -1,28 +1,50 @@
 #!/bin/bash -x
 echo "Welcome to the gambler"
 
-STAKE=100
 BETS=1
+DAY=20
 
-cash=$STAKE
-
-maxLimit=$(($STAKE+(50*$STAKE/100)))
-minLimit=$(($STAKE-(50*$STAKE/100)))
+totalWin=0
+totalLoss=0
+declare -A gamblerDict
 
 function gamblingForDay()
 {
-while [[ $cash -lt $maxLimit && $cash -gt $minLimit ]]
+for (( i=0; i<DAY ;i++ ))
 do
+	stake=100
+	maxLimit=$(($stake+(50*$stake/100)))
+	minLimit=$(($stake-(50*$stake/100)))
+	win=0
+	loss=0
+	while [[ $stake -lt $maxLimit && $stake -gt $minLimit ]]
+	do
 	gamblerResult=$((RANDOM%2))
-	if [ $gamblerResult -eq 0 ]
-	then
-		cash=$(($cash+1))
-	else
-		cash=$(($cash-1))
-	fi
+		if [ $gamblerResult -eq 0 ]
+		then
+			stake=$(($stake+1))
+			win=$(($win+1))
+		else
+			stake=$(($stake-1))
+			loss=$(($loss+1))
+		fi
+	done
+	gamblerDict["DAY$i"]="$stake 	 $win  	$loss"
 
+	totalWin=$(($totalWin+$win))
+	totalLoss=$(($totalLoss+$loss))
 done
-	echo "cash : "$cash
+
+DAY=$(($DAY+1))
+
 }
 
 gamblingForDay
+
+for (( j=0; j<${#gamblerDict[@]}; j++ ))
+do
+	echo "Day $j		${gamblerDict[DAY$j]}"
+done
+
+echo "Total Wins : "$totalWin
+echo "Total Loss : "$totalLoss
