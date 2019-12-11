c20dd2315ee07c285460a287916f807cb8307d10
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-11 12:42:09 +0530

    Added : Daily Limit For Day

--- gambler.sh
+++ gambler.sh
@@ -4,11 +4,25 @@ echo "Welcome to the gambler"
 STAKE=100
 BETS=1
 
-gamblerResult=$((RANDOM%2))
+cash=$STAKE
 
-if [ $gamblerResult -eq 0 ]
-then
-	echo "Win"
-else
-	echo "Loss"
-fi
+maxLimit=$(($STAKE+(50*$STAKE/100)))
+minLimit=$(($STAKE-(50*$STAKE/100)))
+
+function gamblingForDay()
+{
+while [[ $cash -lt $maxLimit && $cash -gt $minLimit ]]
+do
+	gamblerResult=$((RANDOM%2))
+	if [ $gamblerResult -eq 0 ]
+	then
+		cash=$(($cash+1))
+	else
+		cash=$(($cash-1))
+	fi
+
+done
+	echo "cash : "$cash
+}
+
+gamblingForDay
